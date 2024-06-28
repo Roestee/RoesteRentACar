32023702ede746ef2100f8343bb5cdfb73315a85
İbrahim Çınğı <103828984+Roestee@users.noreply.github.com>
2024-06-28 23:19:11 +0300

    update

---
+++ Core/RoesteRentACar.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using RoesteRentACar.Application.Features.CQRS.Queries.AboutQueries;
+using RoesteRentACar.Application.Features.CQRS.Results.AboutResults;
+using RoesteRentACar.Application.Interfaces;
+using RoesteRentACar.Domain.Entities;
+
+namespace RoesteRentACar.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class GetAboutByIdQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutByIdQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetAboutByIdQueryResult> Handle(GetAboutByIdQuery query)
+        {
+            var value = await _repository.GetByIdAsync(query.Id);
+            return new GetAboutByIdQueryResult
+            {
+                Id = value.Id,
+                Title = value.Title,
+                Description = value.Description,
+                ImageUrl = value.ImageUrl
+            };
+        }
+    }
+}
