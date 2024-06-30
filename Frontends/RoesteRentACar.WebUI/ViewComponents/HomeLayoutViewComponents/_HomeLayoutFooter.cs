b5a959b7288d8013b6b4cbc0f117b9fbcd519a5d
İbrahim Çınğı <103828984+Roestee@users.noreply.github.com>
2024-06-30 21:44:30 +0300

    init frontend

---
+++ Frontends/RoesteRentACar.WebUI/ViewComponents/HomeLayoutViewComponents/_HomeLayoutFooter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoesteRentACar.WebUI.ViewComponents.HomeLayoutViewComponents
+{
+    public class _HomeLayoutFooter: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/RoesteRentACar.WebUI/ViewComponents/HomeLayoutViewComponents/_HomeLayoutScripts.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoesteRentACar.WebUI.ViewComponents.HomeLayoutViewComponents
+{
+    public class _HomeLayoutScripts : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
